3aac3c8ea55ee6b20b4718d7e1ecd39fd368cb12
ManuelCanulDev <manuel.canul109@gmail.com>
2021-06-10 20:55:45 -0500

    Sliders

--- curso_inputswidgets/lib/main.dart
+++ curso_inputswidgets/lib/main.dart
@@ -13,13 +13,10 @@ class MyApp extends StatefulWidget {
 
 class _State extends State<MyApp> {
 
-  bool _value1 = false;
+  double _value = 0.0;
 
-  bool _value2 = false;
-
-  void _onChanged1(bool value) => setState(() => _value1 = value);
-  void _onChanged2(bool value) => setState(() => _value2 = value);
-
+  void _setValue(double value) => setState(() => _value = value);
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -30,8 +27,8 @@ class _State extends State<MyApp> {
         padding: new EdgeInsets.all(32.0),
         child: new Column(
           children: <Widget>[
-            new Switch(value: _value1, onChanged: _onChanged1),
-            new SwitchListTile(value: _value2, onChanged: _onChanged2, title: new Text("Hello Word", style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.red),))
+            new Text('VALOR: ${(_value * 100).round()}'),
+            new Slider(value: _value, onChanged: _setValue)
           ],
         ),
       ),
